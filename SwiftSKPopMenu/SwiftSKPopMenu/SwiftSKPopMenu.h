cd73fe7ddccd28b981d37b822d257970c3dc1718
pedrohocastro <pedro_hoc@hotmail.com>
2017-06-02 15:29:59 -0300

    initial commit

---
+++ SwiftSKPopMenu/SwiftSKPopMenu/SwiftSKPopMenu.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSKPopMenu.h
+//  SwiftSKPopMenu
+//
+//  Created by Pedro Oliveira on 02/06/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSKPopMenu.
+FOUNDATION_EXPORT double SwiftSKPopMenuVersionNumber;
+
+//! Project version string for SwiftSKPopMenu.
+FOUNDATION_EXPORT const unsigned char SwiftSKPopMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSKPopMenu/PublicHeader.h>
+
+
