bffaa75380124aad21736d919334c02041a6b160
pedrohocastro <pedro_hoc@hotmail.com>
2017-06-02 15:42:43 -0300

    created SKPopMenuButtonDelegate

---
+++ SwiftSKPopMenu/SwiftSKPopMenu/SKPopMenuButtonDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SKPopMenuButtonDelegate.swift
+//  SwiftSKPopMenu
+//
+//  Created by Pedro Oliveira on 02/06/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+import Foundation
+
+protocol SKPopMenuButtonDelegate{
+    
+}
