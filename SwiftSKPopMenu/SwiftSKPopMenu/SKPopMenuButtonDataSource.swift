4f8ee0b9c8eb17e9b218aa4b01fe807a1d9119c8
FelipeSlim <liipe.frs@gmail.com>
2017-06-02 16:10:26 -0300

    Add protocol SKPopMenuButtonDataSource

---
+++ SwiftSKPopMenu/SwiftSKPopMenu/SKPopMenuButtonDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  SKPopMenuButtonDataSource.swift
+//  SwiftSKPopMenu
+//
+//  Created by Felipe Rodrigues on 02/06/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+import Foundation
+
+protocol SKPopMenuButtonDataSource {
+    
+}
